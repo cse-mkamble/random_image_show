b3738d4c6da33d89189f3ab1a45f66f656b2add3
it-mkamble <82635318+it-mkamble@users.noreply.github.com>
2021-11-13 19:06:18 +0530

    img add in index.html

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('main'));
